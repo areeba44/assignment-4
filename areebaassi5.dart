bb8792a51ea2a6e145051c1ea3892567f213cd16
areeba44 <94166815+areeba44@users.noreply.github.com>
2021-12-03 22:47:15 +0500

    Add files via upload

---
+++ areebaassi5.dart
@@ -0,0 +1,44 @@
+void main(){
+  // q no 1
+  // List a  =["areeba","sana"];
+  // print(a);
+
+  // q no 2
+  // List b  =[1,2,"areeba",true,36,4,"sana",58,7,"apple",12];
+  // print(b);
+
+  // q no 3
+  //  List edu =["SSC","HSC","BCS","BS","BCOM","MS","M.Phil","PHD"];
+  //  print("0 Index:${edu[0]}\n 1 Index:${edu[1]}\n 2 index:${edu[2]}\n 3 index:${edu[3]}\n 4 Index:${edu[4]}\n 5 Index:${edu[5]}\n 6 Index:${edu[6]}\n 7 Index:${edu[7]}");
+
+  //  q no 4
+  // List  name =["areeba","sana","shahla"];
+  // List   marks =[450,467,489];
+  // print(" Index:${name[0]}\n  Index:${marks[0]}\n percantage Index:${marks[0]/500*100}");
+  // print(" Index:${name[1]}\n Index:${marks[1]}\n percantage Index:${marks[1]/500*100}");
+  // print(" Index:${name[2]}\n  Index:${marks[2]}\n percantage Index:${marks[2]/500*100}");
+
+  //  q no 5
+  // List  score=[78,98,88,87,67];
+  // score.sort();  
+  // print(score);
+
+  //  q no 6
+  // List score1=[56,88,54,60];
+  // score1.sort();
+  // print(score1);
+  // var a=List.of(score1.reversed);
+  // print(a);
+
+  //q no 7
+  // List a1=["areeba","sana","areesha","alishba"];
+  // if (a1[2]==""){
+  // print("data present"); 
+  // }
+  // else{
+  //  a1.replaceRange(2, 3, ["apple","orange"]); 
+  //  print(a1);
+  // }
+  
+  
+}
